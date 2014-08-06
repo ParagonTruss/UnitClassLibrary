20ef9eb9737a16e40a56d00ed33df999d7e67bc7
timothyah <timothyaclearspaninc@gmail.com>
2014-08-06 15:00:02 -0400

    Renamed several classes in the unit class library

---
+++ Unit Class Library/ForceUnit.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace UnitClassLibrary
+{
+    [DebuggerDisplay("{Pounds}lbs")]
+    public class ForceUnit : IComparable<ForceUnit>
+    {
+        private const ForceType InternalUnitType = ForceType.Pounds;
+        private double _intrinsicValue = 0.0;
+        public double Pounds
+        {
+            get { return retrieveAsExternalUnit(ForceType.Pounds); }
+        }
+
+        public double Newtons
+        {
+            get { return retrieveAsExternalUnit(ForceType.Newtons); }
+        }
+
+        public double Kips
+        {
+            get { return retrieveAsExternalUnit(ForceType.Kips); }
+        }
+
+
+        #region Constructors
+
+        public ForceUnit()
+        {
+            _intrinsicValue = 0.0;
+        }
+
+        public ForceUnit(ForceType forceType, double passedValue)
+        {
+            storeAsInternalUnit(forceType, passedValue);
+        }
+
+
+        #endregion
+
+        #region helper methods
+        private double retrieveAsExternalUnit(ForceType forceType)
+        {
+            return ConvertTo(InternalUnitType, _intrinsicValue, forceType);
+        }
+
+        private void storeAsInternalUnit(ForceType forceType, double passedValue)
+        {
+            _intrinsicValue = ConvertTo(forceType, passedValue, InternalUnitType);
+        }
+
+
+        public static double ConvertTo(ForceType fromForceType, double passedMagnitude, ForceType toForceType)
+        {
+            double poundMagnitude = 0;
+            double returnMagnitude = 0;
+
+            switch (fromForceType)
+            {
+                case ForceType.Pounds:
+                    poundMagnitude = passedMagnitude;
+                    break;
+                case ForceType.Newtons:
+                    poundMagnitude = passedMagnitude / 4.44822162;
+                    break;
+                case ForceType.Kips:
+                    poundMagnitude = passedMagnitude * 1000;
+                    break;
+                default:
+                    //code should never run
+                    throw new NotSupportedException("Unit not supported!");
+            }
+
+            switch (toForceType)
+            {
+                case ForceType.Pounds:
+                    returnMagnitude = poundMagnitude;
+                    break;
+                case ForceType.Newtons:
+                    returnMagnitude = poundMagnitude * 4.44822162;
+                    break;
+                case ForceType.Kips:
+                    returnMagnitude = poundMagnitude / 1000;
+                    break;
+            }
+
+            return returnMagnitude;
+
+        }
+
+        #endregion
+
+        #region Overloaded Operators
+
+        /* You may notice that we do not overload the increment and decrement operators nor do we overload multiplication and division.
+         * This is because the user of this library does not know what is being internally stored and those operations will not return useful information. 
+         */
+
+        public static ForceUnit operator +(ForceUnit f1, ForceUnit f2)
+        {
+            //add the two Forces together
+            //return a new Force with the new value
+            return new ForceUnit(InternalUnitType, (f1._intrinsicValue + f2._intrinsicValue));
+        }
+
+        public static ForceUnit operator -(ForceUnit f1, ForceUnit f2)
+        {
+            //subtract the two Forces
+            //return a new Force with the new value
+            return new ForceUnit(InternalUnitType, (f1._intrinsicValue - f2._intrinsicValue));
+        }
+
+        public static ForceUnit operator *(ForceUnit f1, double f2)
+        {
+            //multiply a force by a scalar
+            //return a new Force with the new value
+            return new ForceUnit(InternalUnitType, (f1._intrinsicValue * f2));
+        }
+
+        /// <summary>
+        /// Not a perfect equality operator, is only accurate up to Constants.AcceptedEqualityDeviationConstant 
+        /// </summary>
+        public static bool operator ==(ForceUnit f1, ForceUnit f2)
+        {
+            return f1.Equals(f2);
+        }
+
+        /// <summary>
+        /// Not a perfect inequality operator, is only accurate up to Constants.AcceptedEqualityDeviationConstant 
+        /// </summary>
+        public static bool operator !=(ForceUnit f1, ForceUnit f2)
+        {
+            return !f1.Equals(f2);
+        }
+
+        public static bool operator >(ForceUnit f1, ForceUnit f2)
+        {
+            return f1._intrinsicValue > f2._intrinsicValue;
+        }
+
+        public static bool operator <(ForceUnit f1, ForceUnit f2)
+        {
+            return f1._intrinsicValue < f2._intrinsicValue;
+        }
+
+        public static bool operator >=(ForceUnit f1, ForceUnit f2)
+        {
+            return f1.Equals(f2) || f1._intrinsicValue > f2._intrinsicValue;
+        }
+
+        public static bool operator <=(ForceUnit f1, ForceUnit f2)
+        {
+            return f1.Equals(f2) || f1._intrinsicValue < f2._intrinsicValue;
+        }
+
+        /// <summary>
+        /// This override determines how this object is inserted into hashtables.
+        /// </summary>
+        /// <returns>same hashcode as any double would</returns>
+        public override int GetHashCode()
+        {
+            return _intrinsicValue.GetHashCode();
+        }
+
+        /// <summary>
+        /// Makes sure to throw an error telling the user that this is a bad idea
+        /// The Force class does not know what type of unit it contains, 
+        /// (Because it should be thought of containing all unit types) 
+        /// Call Force.[unit].Tostring() instead
+        /// </summary>
+        /// <returns>Should never return anything</returns>
+        public override string ToString()
+        {
+            throw new NotImplementedException("The Force class does not know what type of unit it contains, (Because it should be thought of as containing all unit types) Call Force.[unit].ToString() instead");
+        }
+
+        public string toString(ForceType passedForce)
+        {
+            switch (passedForce)
+            {
+                case ForceType.Pounds:
+                    return _intrinsicValue + " pounds";
+                case ForceType.Newtons:
+                    return ConvertTo(InternalUnitType, _intrinsicValue, ForceType.Newtons) + " newtons";
+                case ForceType.Kips:
+                    return ConvertTo(InternalUnitType, _intrinsicValue, ForceType.Kips) + " kips";
+                default:
+                    // should never be returned
+                    return "Could not determine the unit type";
+            }
+        }
+
+        /// <summary>
+        /// does the same thing as == if the passed in object is a d
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            return Math.Abs(this._intrinsicValue - ((ForceUnit)(obj))._intrinsicValue) < Constants.AcceptedEqualityDeviationConstant;
+        }
+        #endregion
+
+        public int CompareTo(ForceUnit other)
+        {
+            if (this.Equals(other))
+                return 0;
+            else
+                return this._intrinsicValue.CompareTo(other._intrinsicValue);
+        }
+    }
+}
---
+++ Unit Class Library/MomentUnit.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitClassLibrary
+{
+    public class MomentUnit
+    {
+        ForceUnit _force;
+        public ForceUnit Force
+        {
+            get { return _force; }
+            set { _force = value; }
+        }
+
+        Dimension _dimension;
+        public Dimension Dimension
+        {
+            get { return _dimension; }
+            set { _dimension = value; }
+        }
+
+        private double Magnitude
+        {
+            get { return _dimension.Millimeters * _force.Pounds; }
+        }
+
+        public MomentUnit()
+        {
+            _force = new ForceUnit();
+            _dimension = new Dimension();
+        }
+
+        public MomentUnit(ForceUnit passeForce, Dimension passedDimension)
+        {
+            _force = passeForce;
+            _dimension = passedDimension;
+        }
+
+        #region Overloaded Operators
+
+        /* You may notice that we do not overload the increment and decrement operators nor do we overload multiplication and division.
+         * This is because the user of this library does not know what is being internally stored and those operations will not return useful information. 
+         */
+
+        public static MomentUnit operator +(MomentUnit s1, MomentUnit s2)
+        {
+            //add the two Momentes together
+            //return a new Moment with the new value
+            //return new Moment( s1._area + s2._area, s1._force + s2._force);
+            throw new NotImplementedException("Consult engineer: might not be allowed");
+        }
+
+        public static MomentUnit operator -(MomentUnit s1, MomentUnit s2)
+        {
+            //subtract the two Moments
+            //return a new Moment with the new value
+            //return new Moment(s1._area - s2._area, s1._force - s2._force);
+            throw new NotImplementedException("Consult engineer: might not be allowed");
+        }
+
+        /// <summary>
+        /// Not a perfect equality operator, is only accurate up to the Constants.AcceptedEqualityDeviationConstant 
+        /// </summary>
+        public static bool operator ==(MomentUnit s1, MomentUnit s2)
+        {
+            return s1.Equals(s2);
+        }
+
+        /// <summary>
+        /// Not a perfect inequality operator, is only accurate up to Constants.AcceptedEqualityDeviationConstant 
+        /// </summary>
+        public static bool operator !=(MomentUnit s1, MomentUnit s2)
+        {
+            return !s1.Equals(s2);
+        }
+
+        /// <summary>
+        /// judges comparison based on force
+        /// </summary>
+        /// <param name="s1"></param>
+        /// <param name="s2"></param>
+        /// <returns></returns>
+        public static bool operator >(MomentUnit s1, MomentUnit s2)
+        {
+            return s1.Magnitude > s2.Magnitude;
+        }
+
+        public static bool operator <(MomentUnit s1, MomentUnit s2)
+        {
+            return s1.Magnitude < s2.Magnitude;
+        }
+
+        public static bool operator >=(MomentUnit s1, MomentUnit s2)
+        {
+            return s1.Equals(s2) || s1.Magnitude > s2.Magnitude;
+        }
+
+        public static bool operator <=(MomentUnit s1, MomentUnit s2)
+        {
+            return s1.Equals(s2) || s1.Magnitude < s2.Magnitude;
+        }
+
+        /// <summary>
+        /// This override determines how this object is inserted into hashtables.
+        /// </summary>
+        /// <returns>same hashcode as any double would</returns>
+        public override int GetHashCode()
+        {
+            return this.Magnitude.GetHashCode();
+        }
+
+        /// <summary>
+        /// Makes sure to throw an error telling the user that this is a bad idea
+        /// The Moment class does not know what type of unit it contains, 
+        /// (Because it should be thought of containing all unit types) 
+        /// Call Moment.[unit].Tostring() instead
+        /// </summary>
+        /// <returns>Should never return anything</returns>
+        public override string ToString()
+        {
+            throw new NotImplementedException("The Moment class does not know what type of unit it contains, (Because it should be thought of containing all unit types) Call Moment.[unit].ToString() instead");
+        }
+
+        /// <summary>
+        /// if the passed in object is a Moment
+        /// </summary>
+        public override bool Equals(object obj)
+        {
+            try
+            {
+                MomentUnit compare = (MomentUnit)obj;
+
+                return compare._dimension == this._dimension && compare._force == this._force;
+            }
+            catch (InvalidCastException)
+            {
+                return false;
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// This implements the IComparable interface and allows Moments to be sorted and such
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public int CompareTo(MomentUnit other)
+        {
+            // The comparison depends on the comparison of  
+            // the underlying Double values.  
+            if (this.Equals(other))
+                return 0;
+            else
+                return this.Magnitude.CompareTo(other.Magnitude);
+        }
+    }
+}
