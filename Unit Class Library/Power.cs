bb74029fda2b04819c95bcd8d459a5c4124e980a
sehcheese <scotthclearspaninc@gmail.com>
2014-09-11 17:08:13 -0400

    FIlled in Power class and wrote tests for it. Those tests currently pass.

--- Unit Class Library/Power.cs
+++ Unit Class Library/Power.cs
@@ -28,7 +28,131 @@ namespace UnitClassLibrary
 
         #region Helper Methods
 
-        
+        /// <summary>
+        /// Retrieves the power as the unit specified
+        /// </summary>
+        /// <param name="powerType">unit of power</param>
+        /// <returns></returns>
+        private double retrieveAsExternalUnit(PowerType powerType)
+        {
+            return ConvertPower(InternalUnitType, _intrinsicValue, powerType);
+        }
+
+        public static double ConvertPower(PowerType fromPowerType, double _intrinsicValue, PowerType toPowerType)
+        {
+            double returnValue = 0;
+            
+            switch (fromPowerType)
+            {
+                // Convert Watts or Ergs/Second to another unit
+                case PowerType.Watt:
+                case PowerType.ErgsPerSecond:
+                    // An erg/second is simply 1/10000000 of a Watt, so just convert it to Watts and then perfrom the conversion
+                    if (fromPowerType == PowerType.ErgsPerSecond)
+                    {
+                        _intrinsicValue /= 10000000;
+                    }
+                    switch (toPowerType)
+                    {
+                        case PowerType.Watt:
+                            returnValue = _intrinsicValue; // Return given Watts
+                            break;
+                        case PowerType.Horsepower:
+                            returnValue = _intrinsicValue * 0.00134102209; // Convert Watts to Horsepower
+                            break;
+                        case PowerType.FootPoundsPerSecond:
+                            returnValue = _intrinsicValue * 0.737562149; // Convert Watts to FootPounds/Second
+                            break;
+                        case PowerType.MetricHorsepower:
+                            returnValue = _intrinsicValue * 0.00135962162; // Convert Watts to Metric Horsepower
+                            break;
+                        case PowerType.ErgsPerSecond:
+                            returnValue = _intrinsicValue * 10000000; // Convert Watts to Ergs/Second
+                            break;
+                        default:
+                            //code should never run
+                            throw new NotSupportedException("Unit not supported!");
+                    }
+                    break;
+                // Convert Horsepower to another unit
+                case PowerType.Horsepower:
+                    switch (toPowerType)
+                    {
+                        case PowerType.Watt:
+                            returnValue = _intrinsicValue * 745.699872; // Convert Horsepower to Watts
+                            break;
+                        case PowerType.Horsepower:
+                            returnValue = _intrinsicValue; // Return given Horsepower
+                            break;
+                        case PowerType.FootPoundsPerSecond:
+                            returnValue = _intrinsicValue * 550; // Convert Horsepower to FootPounds/Second
+                            break;
+                        case PowerType.MetricHorsepower:
+                            returnValue = _intrinsicValue * 1.01386967; // Convert Horsepower to Metric Horsepower
+                            break;
+                        case PowerType.ErgsPerSecond:
+                            returnValue = _intrinsicValue * 7456998720; // Convert Horsepower to Ergs/Second
+                            break;
+                        default:
+                            //code should never run
+                            throw new NotSupportedException("Unit not supported!");
+                    }
+                    break;
+                // Convert FootPounds/Second to another unit
+                case PowerType.FootPoundsPerSecond:
+                    switch (toPowerType)
+                    {
+                        case PowerType.Watt:
+                            returnValue = _intrinsicValue * 1.35581795; // Convert FootPounds/Second to Watts
+                            break;
+                        case PowerType.Horsepower:
+                            returnValue = _intrinsicValue * 0.00181818182; // Convert FootPounds/Second to Horsepower
+                            break;
+                        case PowerType.FootPoundsPerSecond:
+                            returnValue = _intrinsicValue; // Return given FootPounds/Second
+                            break;
+                        case PowerType.MetricHorsepower:
+                            returnValue = _intrinsicValue * 0.00184339939; // Convert FootPounds/Second to Metric Horsepower
+                            break;
+                        case PowerType.ErgsPerSecond:
+                            returnValue = _intrinsicValue * 13558179.5; // Convert FootPounds/Second to Ergs/Second
+                            break;
+                        default:
+                            //code should never run
+                            throw new NotSupportedException("Unit not supported!");
+                    }
+                    break;
+                // Convert Metric Horsepower to another unit
+                case PowerType.MetricHorsepower:
+                    switch (toPowerType)
+                    {
+                        case PowerType.Watt:
+                            returnValue = _intrinsicValue * 735.49875; // Convert Metric Horsepower to Watts
+                            break;
+                        case PowerType.Horsepower:
+                            returnValue = _intrinsicValue * 0.986320071; // Convert Metric Horsepower to Horsepower
+                            break;
+                        case PowerType.FootPoundsPerSecond:
+                            returnValue = _intrinsicValue * 542.476039; // Convert Metric Horsepower to FootPounds/Second
+                            break;
+                        case PowerType.MetricHorsepower:
+                            returnValue = _intrinsicValue; // Return given Metric Horsepower
+                            break;
+                        case PowerType.ErgsPerSecond:
+                            returnValue = _intrinsicValue * 7354987500; // Convert Metric Horsepower to Ergs/Second
+                            break;
+                        default:
+                            //code should never run
+                            throw new NotSupportedException("Unit not supported!");
+                    }
+                    break;
+                default:
+                    //code should never run
+                    throw new NotSupportedException("Unit not supported!");
+            }
+
+            return returnValue;
+        }
 
         #endregion
 
@@ -40,11 +164,6 @@ namespace UnitClassLibrary
         public double Watt
         {
             get { return retrieveAsExternalUnit(PowerType.Watt); }
-        }
-
-        private double retrieveAsExternalUnit(PowerType powerType)
-        {
-            throw new NotImplementedException();
         }
 
         /// <summary>
--- Unit Library Tests/PowerTests.cs
+++ Unit Library Tests/PowerTests.cs
@@ -45,30 +45,30 @@ namespace Unit_Library_Tests
             double ergsPerSecondToMetricHorsepower = ergsPerSecondPower.MetricHorsepower;
 
             // Assert that gotten values are correct
-            wattsToHorsepower.Should().Be(0.02440660203063);
-            wattsToFootPoundsPerSecond.Should().Be(13.42363111685);
-            wattsToMetricHorsepower.Should().Be(0.02474511);
-            wattsToErgsPerSecond.Should().Be(182000000);
+            wattsToHorsepower.Should().BeApproximately(0.024406602, 0.024406602 * 0.000001); // TODO: change second arg to accepted deviation value FOR ALL OF THESE
+            wattsToFootPoundsPerSecond.Should().BeApproximately(13.42363111685, 13.42363111685 * 0.000001);
+            wattsToMetricHorsepower.Should().BeApproximately(0.02474511, 0.02474511 * 0.000001);
+            wattsToErgsPerSecond.Should().BeApproximately(182000000, 182000000 * 0.000001);
 
-            horsepowerToWatts.Should().Be(13571.7376628);
-            horsepowerToFootPoundsPerSecond.Should().Be(10010);
-            horsepowerToMetricHorsepower.Should().Be(18.45242983);
-            horsepowerToErgsPerSecond.Should().Be(135717376628);
+            horsepowerToWatts.Should().BeApproximately(13571.7376628, 13571.7376628 * 0.000001);
+            horsepowerToFootPoundsPerSecond.Should().BeApproximately(10010, 10010 * 0.000001);
+            horsepowerToMetricHorsepower.Should().BeApproximately(18.45242983, 18.45242983 * 0.000001);
+            horsepowerToErgsPerSecond.Should().BeApproximately(135717376628, 135717376628 * 0.000001);
 
-            footPoundsPerSecondToWatts.Should().Be(24.67588665963);
-            footPoundsPerSecondToHorsepower.Should().Be(0.03309090909091);
-            footPoundsPerSecondToMetricHorsepower.Should().Be(0.0335498689);
-            footPoundsPerSecondToErgsPerSecond.Should().Be(246758866.5963);
+            footPoundsPerSecondToWatts.Should().BeApproximately(24.67588665963, 24.67588665963 * 0.000001);
+            footPoundsPerSecondToHorsepower.Should().BeApproximately(0.03309090909091, 0.03309090909091 * 0.000001);
+            footPoundsPerSecondToMetricHorsepower.Should().BeApproximately(0.0335498689, 0.0335498689 * 0.000001);
+            footPoundsPerSecondToErgsPerSecond.Should().BeApproximately(246758866.5963, 246758866.5963 * 0.000001);
 
-            metricHorsepowerToWatts.Should().Be(13386.0772);
-            metricHorsepowerToHorsepower.Should().Be(17.9510253);
-            metricHorsepowerToFootPoundsPerSecond.Should().Be(9873.06391);
-            metricHorsepowerToErgsPerSecond.Should().Be(133860772500);
+            metricHorsepowerToWatts.Should().BeApproximately(13386.0772, 13386.0772 * 0.000001);
+            metricHorsepowerToHorsepower.Should().BeApproximately(17.9510253, 17.9510253 * 0.000001);
+            metricHorsepowerToFootPoundsPerSecond.Should().BeApproximately(9873.06391, 9873.06391 * 0.000001);
+            metricHorsepowerToErgsPerSecond.Should().BeApproximately(133860772500, 133860772500 * 0.000001);
 
             ergsPerSecondToWatts.Should().Be(182);
-            ergsPerSecondToHorsepower.Should().Be(0.2440660203063);
-            ergsPerSecondToFootPoundsPerSecond.Should().Be(134.2363111685);
-            ergsPerSecondToMetricHorsepower.Should().Be(0.24745112);
+            ergsPerSecondToHorsepower.Should().BeApproximately(0.2440660203063, 0.2440660203063 * 0.000001);
+            ergsPerSecondToFootPoundsPerSecond.Should().BeApproximately(134.2363111685, 134.2363111685 * 0.000001);
+            ergsPerSecondToMetricHorsepower.Should().BeApproximately(0.24745112, 0.24745112 * 0.000001);
         }
     }
 }
