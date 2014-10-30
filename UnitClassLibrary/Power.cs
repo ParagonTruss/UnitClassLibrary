043bd5cd7f215183b8752b9e67ab10a5df43fcd8
Brendan Chatterton <brendancclearspaninc@gmail.com>
2014-10-30 12:03:06 -0400

    Volume Class Update and Related Updates

--- UnitClassLibrary/Power.cs
+++ UnitClassLibrary/Power.cs
@@ -145,7 +145,7 @@ namespace UnitClassLibrary
         /// </summary>
         /// <param name="p1">power one</param>
         /// <param name="p2">power two</param>
-        /// <returns>the sum of the two forces</returns>
+        /// <returns>the difference of the two forces</returns>
         public static Power operator -(Power p1, Power p2)
         {
             return new Power(p1._energy - p2._energy, p1._time - p2._time);
