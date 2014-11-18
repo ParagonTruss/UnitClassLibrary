4c08a92c0385cfd34309b88193f87d3afd13e287
jth41 <thomas.holland.john@gmail.com>
2014-11-18 09:56:21 -0500

    fixed xml error

--- UnitClassLibrary/Volume.cs
+++ UnitClassLibrary/Volume.cs
@@ -1160,7 +1160,7 @@ namespace UnitClassLibrary
         /// This implements the IComparable interface and allows Volumes to be sorted and such
         /// </summary>
         /// <param name="other">volume to check against</param>
-        /// <returns>0 if equal; 1 if this > other; -1 if this < other</returns>
+        /// <returns>0 if equal; 1 if this greater than other; -1 if this less than other </returns>
         public int CompareTo(Volume other)
         {
             if (this.Equals(other))
