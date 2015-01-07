794c9736920defb86dde8900203607b9ad935160
Jake Wasdin <jakewclearspaninc@gmail.com>
2015-01-07 11:09:24 -0500

    adding passing unit test for absolute value for force units

--- UnitLibraryTests/ForceTests.cs
+++ UnitLibraryTests/ForceTests.cs
@@ -28,5 +28,22 @@ namespace UnitLibraryTests
             kip.Should().Be(0.1);
         }
 
+        [Test()]
+        public void Force_AbsoluteValueTests()
+        {
+            // new units
+            ForceUnit negativeForce = new ForceUnit(ForceType.Pounds, -100);
+            ForceUnit positiveForce = new ForceUnit(ForceType.Pounds, 100);
+
+            // get absolute values
+            double negativeAbs = negativeForce.GetAbsoluteValue(ForceType.Pounds);
+            double positiveAbs = positiveForce.GetAbsoluteValue(ForceType.Pounds);
+
+            // assert
+            negativeAbs.Should().Be(100);
+            positiveAbs.Should().Be(100);
+            negativeAbs.ShouldBeEquivalentTo(positiveAbs);
+        }
+
     }
 }
