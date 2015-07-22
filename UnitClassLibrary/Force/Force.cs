1a8964eec6267d31a225dff2fb238e938b1a3ef3
sehcheese <scotthclearspaninc@gmail.com>
2015-07-22 13:17:52 -0400

    Add JSON serialization to Force.

--- UnitClassLibrary/Force/Force.cs
+++ UnitClassLibrary/Force/Force.cs
@@ -1,18 +1,22 @@
 using System;
+using Newtonsoft.Json;
 
  namespace UnitClassLibrary
 {
-
+    [JsonObject(MemberSerialization.OptIn)]
 	public partial class Force
 	{
 		#region _fields and Internal Properties
 
+        
 		internal ForceType InternalUnitType
 		{
 			get { return _internalUnitType; }
 		}
+        [JsonProperty]
 		private ForceType _internalUnitType;
 
+        [JsonProperty]
 		private double _intrinsicValue;
 
 		public ForceEqualityStrategy EqualityStrategy
@@ -36,10 +40,11 @@ using System;
 		}
 
 		/// <summary> Accepts standard types for input. </summary>
-		public Force(ForceType passedForceType, double passedInput, ForceEqualityStrategy passedStrategy = null)
+        [JsonConstructor]
+		public Force(ForceType internalUnitType, double intrinsicValue, ForceEqualityStrategy passedStrategy = null)
 		{
-			_intrinsicValue = passedInput;
-			_internalUnitType = passedForceType;
+            _intrinsicValue = intrinsicValue;
+            _internalUnitType = internalUnitType;
 			_equalityStrategy = _chooseDefaultOrPassedStrategy(passedStrategy);
 		}
 
